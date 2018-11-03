ef4acc86892cc71911ce5ab268857aa5c0eefb3e
atli15 <atli15@itu.edu.tr>
2018-11-03 13:16:35 +0300

    hello

---
+++ app/src/main/java/com/example/xxx/easymoney/EasyMoney.java
@@ -0,0 +1,13 @@
+package com.example.xxx.easymoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EasyMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_money);
+    }
+}

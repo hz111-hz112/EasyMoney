e74fbad4b6bc007906ad69be796ce84bc8691116
ozata <ozata1@itu.edu.tr>
2018-11-03 13:34:13 +0300

    jack first change

--- app/src/main/java/com/example/xxx/easymoney/EasyMoney.java
+++ app/src/main/java/com/example/xxx/easymoney/EasyMoney.java
@@ -1,7 +1,9 @@
 package com.example.xxx.easymoney;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EasyMoney extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class EasyMoney extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_money);
     }
+
+
+    public void jackWelcome(View view){
+        Intent intent = new Intent(EasyMoney.this, HandsomeJack.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/xxx/easymoney/HandsomeJack.java
@@ -0,0 +1,19 @@
+package com.example.xxx.easymoney;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandsomeJack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handsome_jack);
+    }
+
+
+
+
+
+}
